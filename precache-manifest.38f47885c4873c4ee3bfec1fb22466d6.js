a8d87d4a58e22494cb0957baddb54f162e837d0d
BALASHEKHAR <kamandlabalashekhar@gmail.com>
2021-07-12 12:17:33 +0530

    gh-pages

---
+++ precache-manifest.38f47885c4873c4ee3bfec1fb22466d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb89f26ceda6dd6b7f957d3ec5445e4e",
+    "url": "/More-to-store-admin-panel/index.html"
+  },
+  {
+    "revision": "706869ab87e8ee989652",
+    "url": "/More-to-store-admin-panel/static/css/main.e2e9fc67.chunk.css"
+  },
+  {
+    "revision": "0396151c1fce826bda11",
+    "url": "/More-to-store-admin-panel/static/js/2.c258f460.chunk.js"
+  },
+  {
+    "revision": "c9f6628721da4be8b98210c9f369466f",
+    "url": "/More-to-store-admin-panel/static/js/2.c258f460.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "706869ab87e8ee989652",
+    "url": "/More-to-store-admin-panel/static/js/main.9f204e32.chunk.js"
+  },
+  {
+    "revision": "b3e7f159ee27c95d7ac6",
+    "url": "/More-to-store-admin-panel/static/js/runtime-main.bfe04f85.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/More-to-store-admin-panel/precache-manifest.38f47885c4873c4ee3bfec1fb22466d6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/More-to-store-admin-panel/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
